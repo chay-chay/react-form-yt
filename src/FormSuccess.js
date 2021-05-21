727cbd1eea90a63105740d8011b934b2a21c31d4
Chayanit McCormick <chayanit.chaisri@gmail.com>
2021-05-21 13:45:26 -0400

    Adding FormSuccess and images

---
+++ src/FormSuccess.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const FormSuccess = () => {
+    return (
+        <div>
+            <div className='form-control-right'>
+                <div className='form-success'>We have received your request!</div>
+                <img src='img/img-3.svg' alt='success' className='form-img-2' />
+            </div>
+        </div>
+    )
+}
+
+export default FormSuccess
